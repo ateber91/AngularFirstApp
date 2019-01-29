47314f29a7ac34a0b645c9c08bce1bf040ad4ae1
ateber91 <ateber91@gmail.com>
2019-01-29 19:46:02 +0200

    components added.

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<app-server></app-server>'
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
